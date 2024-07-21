959c089a87505e38fd01f7f9e042ac6092f8cce6
Canul <canuladriana24@gmail.com>
2024-07-20 21:50:21 -0600

    primer commit

---
+++ app/src/main/java/com/example/bd_app/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.bd_app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText usuarioEditText;
+    EditText passwordEditText;
+    Button iniciarButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usuarioEditText = findViewById(R.id.usuario);
+        passwordEditText = findViewById(R.id.password);
+        iniciarButton = findViewById(R.id.iniciar);
+
+        iniciarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = usuarioEditText.getText().toString();
+                String pass = passwordEditText.getText().toString();
+
+                if (user.equals("Adriana") && pass.equals("adriana")) {
+                    SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                    editor.putString("usuario", user);
+                    editor.apply();
+
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    intent.putExtra("usuario", user);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
